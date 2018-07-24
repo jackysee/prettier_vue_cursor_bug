2669b20b85ca1cf82e8c1c05fc998e805c8a6d15
jackysee <jackysee@gmail.com>
2018-07-24 17:51:40 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+export default {
+
+          a(){
+              return "1";
+          }
+
+}
---
+++ index.vue
@@ -0,0 +1,20 @@
+<style>
+</style>
+
+
+<template>
+<div> Hello </div>
+</template>
+
+
+<script>
+
+export default {
+
+                        a(){ 
+                            return "1";
+                        }
+
+}
+
+</script>
